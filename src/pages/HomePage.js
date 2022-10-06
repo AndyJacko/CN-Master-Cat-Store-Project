f0cce82dd780505552836d1daec95a6026c82fd2
horsellcommon <crawfordj716@gmail.com>
2022-10-06 15:24:16 +0100

    Removed API component and moved code into HomePage.js. Added useState for loading as API is fetched before anything is displayed. Added temporary CSS file for loading animation.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,162 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ProductDetail from "./ProductDetail";
+import styled from "styled-components";
+import "./Homepage.css";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  let [data, setData] = useState();
+  const [isLoading, setisLoading] = useState(true);
+  let catArray = [];
+  let url = [
+    "https://api.thecatapi.com/v1/images/BDb8ZXb1v",
+    "https://api.thecatapi.com/v1/images/B1ERTmgph",
+    "https://api.thecatapi.com/v1/images/DbwiefiaY",
+    "https://api.thecatapi.com/v1/images/4RzEwvyzz",
+    "https://api.thecatapi.com/v1/images/OOD3VXAQn",
+    "https://api.thecatapi.com/v1/images/sxIXJax6h",
+    "https://api.thecatapi.com/v1/images/-tm9-znzl",
+    "https://api.thecatapi.com/v1/images/TuSyTkt2n",
+    "https://api.thecatapi.com/v1/images/IFXsxmXLm",
+    "https://api.thecatapi.com/v1/images/j6oFGLpRG",
+    "https://api.thecatapi.com/v1/images/njK25knLH",
+    "https://api.thecatapi.com/v1/images/ai6Jps4sx",
+  ];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      for (let i = 0; i < url.length; i++) {
+        try {
+          let one = await fetch(url[i]);
+          let two = await one.json();
+          catArray.push(two);
+        } catch (error) {
+          console.log("Error");
+        } finally {
+          console.log(data);
+        }
+      }
+      setisLoading(false);
+      setData(catArray);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {isLoading && <div className="loader"></div>}
+      {data && !isLoading && (
+        <div>
+          <div className="grouped">
+            {<h2>Breed: {data[0].breeds[0].name}</h2>}
+            {<h3>Alternative names: {data[0].breeds[0].alt_names}</h3>}
+            {<img src={data[0].url} width="200" height="150" alt="Sphynx" />}
+            <Link to={ProductDetail}>View More Details</Link>
+
+            {<h2>Breed: {data[1].breeds[0].name}</h2>}
+            {<h3>Alternative names: {data[1].breeds[0].alt_names}</h3>}
+            {
+              <img
+                src={data[1].url}
+                width="200"
+                height="150"
+                alt="California Spangled"
+              />
+            }
+            <Link to={ProductDetail}>View More Details</Link>
+
+            {<h2>Breed: {data[2].breeds[0].name}</h2>}
+            {<h3>Alternative names: {data[2].breeds[0].alt_names}</h3>}
+            {<img src={data[2].url} width="200" height="150" alt="Korat" />}
+            <Link to={ProductDetail}>View More Details</Link>
+
+            {<h2>Breed: {data[3].breeds[0].name}</h2>}
+            {<h3>Alternative names: {data[3].breeds[0].alt_names}</h3>}
+            {<img src={data[3].url} width="200" height="150" alt="Devon Rex" />}
+            <Link to={ProductDetail}>View More Details</Link>
+          </div>
+          <div className="grouped">
+            {<h2>Breed: {data[4].breeds[0].name}</h2>}
+            {<h3>Alternative names: {data[4].breeds[0].alt_names}</h3>}
+            {
+              <img
+                src={data[4].url}
+                width="200"
+                height="150"
+                alt="Maine Coon"
+              />
+            }
+            <Link to={ProductDetail}>View More Details</Link>
+
+            {<h2>Breed: {data[5].breeds[0].name}</h2>}
+            {<h3>Alternative names: {data[5].breeds[0].alt_names}</h3>}
+            {
+              <img
+                src={data[5].url}
+                width="200"
+                height="150"
+                alt="Turkish Van"
+              />
+            }
+            <Link to={ProductDetail}>View More Details</Link>
+
+            {<h2>Breed: {data[6].breeds[0].name}</h2>}
+            {<h3>Alternative names: {data[6].breeds[0].alt_names}</h3>}
+            {
+              <img
+                src={data[6].url}
+                width="200"
+                height="150"
+                alt="Japanese Bobtail"
+              />
+            }
+            <Link to={ProductDetail}>View More Details</Link>
+
+            {<h2>Breed: {data[7].breeds[0].name}</h2>}
+            {<h3>Alternative names: {data[7].breeds[0].alt_names}</h3>}
+            {
+              <img
+                src={data[7].url}
+                width="200"
+                height="150"
+                alt="Egyptian Mau"
+              />
+            }
+            <Link to={ProductDetail}>View More Details</Link>
+          </div>
+          <div className="grouped">
+            {<h2>Breed: {data[8].breeds[0].name}</h2>}
+            {<h3>Alternative names: {data[8].breeds[0].alt_names}</h3>}
+            {<img src={data[8].url} width="200" height="150" alt="Bengal" />}
+            <Link to={ProductDetail}>View More Details</Link>
+
+            {<h2>Breed: {data[9].breeds[0].name}</h2>}
+            {<h3>Alternative names: {data[9].breeds[0].alt_names}</h3>}
+            {<img src={data[9].url} width="200" height="150" alt="Chartreux" />}
+            <Link to={ProductDetail}>View More Details</Link>
+
+            {<h2>Breed: {data[10].breeds[0].name}</h2>}
+            {<h3>Alternative names: {data[10].breeds[0].alt_names}</h3>}
+            {
+              <img
+                src={data[10].url}
+                width="200"
+                height="150"
+                alt="Havana Brown"
+              />
+            }
+            <Link to={ProductDetail}>View More Details</Link>
+
+            {<h2>Breed: {data[11].breeds[0].name}</h2>}
+            {<h3>Alternative names: {data[11].breeds[0].alt_names}</h3>}
+            {<img src={data[11].url} width="200" height="150" alt="Siamese" />}
+            <Link to={ProductDetail}>View More Details</Link>
+          </div>
+        </div>
+      )}{" "}
+    </>
+  );
 };
 
 export default HomePage;

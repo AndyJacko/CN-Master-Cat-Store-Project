5e55210a8cca3573dbcba52456ec5ad25dacdfbf
horsellcommon <crawfordj716@gmail.com>
2022-10-06 12:10:16 +0100

    Refactored API even more, created for loop to reduce amount of code

--- src/components/API.js
+++ src/components/API.js
@@ -1,64 +1,27 @@
 import { useEffect, useState } from "react";
 
 export const CatAPI = () => {
-  const [data, setData] = useState([]);
-  const catArray = [];
+  let [data, setData] = useState([]);
+  let catArray = [];
+  let url = ["https://api.thecatapi.com/v1/images/BDb8ZXb1v", "https://api.thecatapi.com/v1/images/B1ERTmgph", "https://api.thecatapi.com/v1/images/DbwiefiaY", "https://api.thecatapi.com/v1/images/4RzEwvyzz", "https://api.thecatapi.com/v1/images/OOD3VXAQn", "https://api.thecatapi.com/v1/images/sxIXJax6h", "https://api.thecatapi.com/v1/images/-tm9-znzl", "https://api.thecatapi.com/v1/images/TuSyTkt2n", "https://api.thecatapi.com/v1/images/IFXsxmXLm", "https://api.thecatapi.com/v1/images/j6oFGLpRG", "https://api.thecatapi.com/v1/images/njK25knLH", "https://api.thecatapi.com/v1/images/ai6Jps4sx"]
 
   useEffect(() => {
     const fetchData = async () => {
-      let response = await fetch(
-        "https://api.thecatapi.com/v1/images/BDb8ZXb1v" // sphy
-      );
-      let data = await response.json();
-      setData(data);
-      catArray.push(data);
-      response = await fetch("https://api.thecatapi.com/v1/images/B1ERTmgph");
-      data = await response.json();
-      setData(data);
-      catArray.push(data);
-      response = await fetch("https://api.thecatapi.com/v1/images/DbwiefiaY");
-      data = await response.json();
-      setData(data);
-      catArray.push(data);
-      response = await fetch("https://api.thecatapi.com/v1/images/4RzEwvyzz");
-      data = await response.json();
-      setData(data);
-      catArray.push(data);
-      response = await fetch("https://api.thecatapi.com/v1/images/OOD3VXAQn");
-      data = await response.json();
-      setData(data);
-      catArray.push(data);
-      response = await fetch("https://api.thecatapi.com/v1/images/sxIXJax6h");
-      data = await response.json();
-      setData(data);
-      catArray.push(data);
-      response = await fetch("https://api.thecatapi.com/v1/images/-tm9-znzl");
-      data = await response.json();
-      setData(data);
-      catArray.push(data);
-      response = await fetch("https://api.thecatapi.com/v1/images/TuSyTkt2n");
-      data = await response.json();
-      setData(data);
-      catArray.push(data);
-      response = await fetch("https://api.thecatapi.com/v1/images/IFXsxmXLm");
-      data = await response.json();
-      setData(data);
-      catArray.push(data);
-      response = await fetch("https://api.thecatapi.com/v1/images/j6oFGLpRG");
-      data = await response.json();
-      setData(data);
-      catArray.push(data);
-      response = await fetch("https://api.thecatapi.com/v1/images/njK25knLH");
-      data = await response.json();
-      setData(data);
-      catArray.push(data);
-      response = await fetch("https://api.thecatapi.com/v1/images/ai6Jps4sx");
-      data = await response.json();
-      setData(data);
-      catArray.push(data);
 
-      console.log(catArray);
-    };
+      for (let i = 0; i < url.length; i++) {
+        try {
+          let one = await fetch(url[i])
+          let two = await one.json()
+          catArray.push(two)
+        } catch (error) {
+          console.log("Error")
+        } finally {
+          data = catArray
+          setData(data)
+          console.log(data)
+        }
+      }
+    }
     fetchData();
   }, []);
 };
